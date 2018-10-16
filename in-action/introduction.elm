28d3d29326e98e101147eb78ce0183f3e3b69506
Simon <simon.jang.contact@gmail.com>
2018-10-16 21:23:23 +0200

    add Record, Collections and Tuple examples

--- in-action/introduction.elm
+++ in-action/introduction.elm
@@ -1,3 +1,7 @@
+import List
+import Tuple
+import Basics
+
 -- Example String Filter + Function
 -- String.filter is a higher order function that accepts other functions as arguments
 
@@ -36,3 +40,33 @@ String.filter (\char -> Char.isDigit char) "1a1a" -- Will return 11
 -- Operators are functions
 
 (/) 7 2 -- Will return 3.5
+
+-- Array and Lists don't have method on the object but require to use the methods from the module\
+-- All the methods of Array and List live in their respective module
+
+List.length [1, 2, 3] -- returns 3
+[1, 2] ++ [3, 4] -- concats 2 lists together
+0 :: List.range 1 5 -- returns [0, 1, 2, 3, 4, 5]
+
+-- Records aka Objects
+
+user = { name = "Simon", firstName = "Foo" }
+user.name -- returns "Simon"
+
+newUser = {user | name = "newUser"} -- Creates a new value based on an existing record and overwrites a specific field
+
+-- Tuples
+-- Why tuples over records? A `tuple` is a record-like value whose fields are accessed by position rather than by name
+-- You cannot access tuples by their named fields
+-- Tuples or more then 3 elements are not accepted in Elm
+
+myTuple = ("Simon", 30)
+
+-- These functions only work on tuples with 2 records
+Tuple.first myTuple -- will return "Simon"
+Tuple.second myTuple -- will return 30
+
+-- When the tuples have more then 2 fields, destructuring is needed
+multiplyBy3D (x, y, z) = x * y * z
+
+multiply2D someTuple = let (x, y) = someTuple in x * y
